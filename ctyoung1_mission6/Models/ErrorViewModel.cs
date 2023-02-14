5a736603cd42abb99dffe8461f74626e849d580e
ctyoung4ever <79294074+ctyoung4ever@users.noreply.github.com>
2023-02-13 22:08:11 -0700

    Add project files.

---
+++ ctyoung1_mission6/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ctyoung1_mission6.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
